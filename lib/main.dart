02e7f5d81c4e151d01c0a9f982246ee783978831
ParkGiraffe <op5321@soongsil.ac.kr>
2024-05-14 00:28:19 +0900

    Format: HorizonList  추출

--- lib/main.dart
+++ lib/main.dart
@@ -32,110 +32,10 @@ class MyApp extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         const SectionTitle(titleText: '최근에 추가된 굿즈들'),
-                        SizedBox(
+                        const SizedBox(
                           height: 10,
                         ),
-                        //user list
-                        Container(
-                          child: SingleChildScrollView(
-                            scrollDirection: Axis.horizontal,
-                            child: Row(
-                              children: [
-                                Container(
-                                  height: 300,
-                                  width: 250,
-                                  margin: EdgeInsets.only(right: 30),
-                                  decoration: BoxDecoration(
-                                    color: Colors.grey,
-                                    borderRadius: BorderRadius.circular(15),
-                                    image: DecorationImage(
-                                      fit: BoxFit.cover,
-                                      image: AssetImage(
-                                        'assets/goods.jpeg',
-                                      ),
-                                    ),
-                                  ),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.end,
-                                    children: [
-                                      Container(
-                                        height: 100,
-                                        width: 250,
-                                        decoration: const BoxDecoration(
-                                          color: Color(0xDEDBCACA),
-                                          borderRadius: BorderRadius.only(
-                                            bottomLeft: Radius.circular(15),
-                                            bottomRight: Radius.circular(15),
-                                          ),
-                                        ),
-                                        child: Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: [
-                                            Text(
-                                              'GoodsName',
-                                              style: TextStyle(
-                                                fontSize: 18,
-                                                fontWeight: FontWeight.bold,
-                                              ),
-                                            ),
-                                            Text('2024.xx.xx'),
-                                          ],
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Container(
-                                  height: 300,
-                                  width: 250,
-                                  margin: EdgeInsets.only(right: 30),
-                                  decoration: BoxDecoration(
-                                    color: Colors.grey,
-                                    borderRadius: BorderRadius.circular(15),
-                                    image: DecorationImage(
-                                      fit: BoxFit.cover,
-                                      image: AssetImage(
-                                        'assets/goods.jpeg',
-                                      ),
-                                    ),
-                                  ),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.end,
-                                    children: [
-                                      Container(
-                                        height: 100,
-                                        width: 250,
-                                        decoration: const BoxDecoration(
-                                          color: Color(0xDEDBCACA),
-                                          borderRadius: BorderRadius.only(
-                                            bottomLeft: Radius.circular(15),
-                                            bottomRight: Radius.circular(15),
-                                          ),
-                                        ),
-                                        child: Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: [
-                                            Text(
-                                              'GoodsName',
-                                              style: TextStyle(
-                                                fontSize: 18,
-                                                fontWeight: FontWeight.bold,
-                                              ),
-                                            ),
-                                            Text('2024.xx.xx'),
-                                          ],
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-
+                        HorizonList(),
                         const SizedBox(
                           height: 20,
                         ),
@@ -271,6 +171,94 @@ class MyApp extends StatelessWidget {
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class HorizonList extends StatelessWidget {
+  const HorizonList({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          HorizonListEl(
+            imageRoute: 'assets/goods.jpeg',
+            goodsName: 'GoodsName',
+            date: '2024.xx.xx',
+          ),
+          HorizonListEl(
+            imageRoute: 'assets/goods.jpeg',
+            goodsName: 'GoodsName',
+            date: '2024.xx.xx',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HorizonListEl extends StatelessWidget {
+  final String imageRoute;
+  final String goodsName;
+  final String date;
+
+  const HorizonListEl({
+    super.key,
+    required this.imageRoute,
+    required this.goodsName,
+    required this.date,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      width: 250,
+      margin: const EdgeInsets.only(right: 30),
+      decoration: BoxDecoration(
+        color: Colors.grey,
+        borderRadius: BorderRadius.circular(15),
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage(
+            imageRoute,
+          ),
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(
+            height: 100,
+            width: 250,
+            decoration: const BoxDecoration(
+              color: Color(0xDEDBCACA),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(15),
+                bottomRight: Radius.circular(15),
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  goodsName,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(date),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
