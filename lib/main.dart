06fe62f31c52f301d468b8be958804d0e49d40c3
ParkGiraffe <op5321@soongsil.ac.kr>
2024-05-14 15:36:42 +0900

    Feat: wishlist 페이지 제작

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:goodwishes/pages/wishlist_main_page.dart';
 import 'package:goodwishes/widgets/bottom_navigation.dart';
-import 'package:goodwishes/widgets/section_title.dart';
-import 'package:goodwishes/widgets/top_with_profile.dart';
-import 'package:goodwishes/widgets/horizon_list.dart';
-import 'package:goodwishes/widgets/category_list.dart';
+import 'package:goodwishes/pages/goods_main_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,44 +18,7 @@ class MyApp extends StatelessWidget {
         child: Scaffold(
           bottomNavigationBar: BottomNavigation(),
           body: SafeArea(
-            child: Padding(
-              padding: EdgeInsets.only(left: 30),
-              child: SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 20,
-                    ),
-                    // top
-                    TopWithProfile(),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    // body
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SectionTitle(titleText: '최근에 추가된 굿즈들'),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        HorizonList(),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        // feeds
-                        SectionTitle(titleText: 'Category'),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        CategoryList(),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            child: WishlistMainPage(),
           ),
         ),
       ),
